895135cafd8199c79adb075776bde3fd391434e7
Vinay Dev S <vinaydevs@gmail.com>
2024-06-11 19:17:09 +0530

    fixes build bug

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,8 +37,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            I'm a full stack developer with a flair for crafting dynamic and
-            immersive web experiences.
+            I&apos;m a full stack developer with a flair for crafting dynamic
+            and immersive web experiences.
           </p>
           <div>
             <Link
